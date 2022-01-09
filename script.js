bef2770a0f1a3943868facb70747cc8729e02a6e
Yael Kalach <ylklk13579@gmail.com>
2022-01-09 14:33:12 +0200

    css complete

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const openFormButton = document.querySelector('.profile__button_edit');
 const popup = document.querySelector('.popup');
 const closeButton = popup.querySelector('.popup__close-button');
 const form = document.querySelector('.popup__form');
+const likeButton = document.querySelectorAll('.elements__like-button');
 
 const profileName = document.querySelector('.profile__name');
 const profileAbout = document.querySelector('.profile__about');
@@ -12,6 +13,7 @@ const inputAbout = document.querySelector('.popup__form_input_about');
 openFormButton.addEventListener('click', toggleForm);
 closeButton.addEventListener('click', closeForm); 
 
+//popup
 
 function toggleForm() {
   inputName.value = profileName.textContent;
@@ -23,6 +25,8 @@ function closeForm() {
   popup.classList.remove('popup_visible');
 }
 
+//form
+
 function handleFormSubmit(event) {
   event.preventDefault();
   profileName.textContent = inputName.value;
@@ -31,3 +35,18 @@ function handleFormSubmit(event) {
 
 form.addEventListener('submit', handleFormSubmit); 
 form.addEventListener('submit', closeForm); 
+
+//like button
+
+function likeClick() {
+  likeButton.classList.remove('elements__like-button_inacative');
+  likeButton.classList.add('elements__like-button_active');
+}
+
+function unlikeClick() {
+  likeButton.classList.remove('elements__like-button_active');
+  likeButton.classList.add('elements__like-button_inacative');
+}
+
+likeButton.addEventListener('click', likeClick);
+likeButton.addEventListener('click', unlikeClick);
