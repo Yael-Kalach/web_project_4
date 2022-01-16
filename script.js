6e7e6a2fe95bf0b4e4b89e1df9c146dfbb0ddd8e
Yael Kalach <ylklk13579@gmail.com>
2022-01-16 19:03:00 +0200

    started on project 5

--- script.js
+++ script.js
@@ -1,38 +1,95 @@
-const openFormButton = document.querySelector('.profile__button_type_edit');
+const openEditFormButton = document.querySelector('.profile__button_type_edit');
+const openAddFormButton = document.querySelector('.profile__button_type_add');
 const popup = document.querySelector('.popup');
+const editPopup = document.querySelector('.edit__popup');
+const addPopup = document.querySelector('.add__popup');
 const closeButton = popup.querySelector('.popup__close-button');
-const form = document.querySelector('.form');
+const editCloseButton = editPopup.querySelector('.edit-popup__close-button');
+const addCloseButton = addPopup.querySelector('.add-popup__close-button');
+const editForm = document.querySelector('.form__edit');
+const addForm = document.querySelector('.form__add');
+const elementsContainer = document.querySelector('.elements__container');
 
+// edit
 const profileName = document.querySelector('.profile__name');
 const profileAbout = document.querySelector('.profile__about');
-
+// edit input
 const inputName = document.querySelector('.form__input_type_name');
 const inputAbout = document.querySelector('.form__input_type_about');
 
-//popup
+// Edit popup
 
-function openForm() {
+function openEditForm() {
   inputName.value = profileName.textContent;
   inputAbout.value = profileAbout.textContent;
-  popup.classList.add('popup_visible');
+  editPopup.classList.add('popup_visible');
 }
 
-function closeForm() {
-  popup.classList.remove('popup_visible');
+function closeEditForm() {
+  editPopup.classList.remove('popup_visible');
 }
 
-openFormButton.addEventListener('click', openForm);
-closeButton.addEventListener('click', closeForm); 
+openEditFormButton.addEventListener('click', openEditForm);
+editCloseButton.addEventListener('click', closeEditForm); 
 
-//form
+// Edit form
 
 function handleFormSubmit(event) {
   event.preventDefault();
   profileName.textContent = inputName.value;
   profileAbout.textContent = inputAbout.value;
-  closeForm()
+  closeEditForm()
 }
 
-form.addEventListener('submit', handleFormSubmit); 
+editForm.addEventListener('submit', handleFormSubmit); 
 
-//like button
+// add
+const elementsTitle = document.querySelector('.elements__text');
+const elementsImage = document.querySelector('.elements__image');
+// add input
+const inputTitle = document.querySelector('.form__input_type_title');
+const inputImage = document.querySelector('.form__input_type_image');
+
+// add popup
+
+function openAddForm() {
+  addPopup.classList.add('popup_visible');
+}
+
+function closeAddForm() {
+  addPopup.classList.remove('popup_visible');
+}
+
+openAddFormButton.addEventListener('click', openAddForm);
+addCloseButton.addEventListener('click', closeAddForm); 
+
+// Elements
+
+function addElement(imageValue, textValue) {
+  // template
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.elements__card').cloneNode(true);
+  // contents
+  cardElement.querySelector('.elements__image').textContent = imageValue;
+  cardElement.querySelector('.elements__text').textContent = textValue;
+  // trash button
+  cardElement.querySelector('.elements__button_trash').addEventListener("click", function (evt) {
+    evt.cardElement.remove();})
+  // like button
+  cardElement.querySelector('.elements__button_like').addEventListener("click", function (evt) {
+    evt.target.classList.toggle('elements__button_like_active');
+  });
+  elementsContainer.append(cardElement);
+}
+
+// add form
+
+function handleCardSubmit(event) {
+  event.preventDefault();
+  elementsTitle.textContent = inputTitle.value;
+  elementsImage.textContent = inputImage.value;
+  closeAddForm()
+  addElement()
+}
+
+addForm.addEventListener('submit', handleCardSubmit);
